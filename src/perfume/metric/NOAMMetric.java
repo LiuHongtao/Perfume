654b8eaa7ef1c80bc39d9636081dd160861045bb
LiuHongtao <lht91225@gmail.com>
2015-11-09 17:07:22 +0800

    NOAM

--- src/perfume/metric/NOAMMetric.java
+++ src/perfume/metric/NOAMMetric.java
@@ -1,9 +1,110 @@
 package perfume.metric;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.FieldAccess;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jdt.core.dom.ReturnStatement;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+
+import perfume.util.ast.InstanceOfUtil;
+
+/**
+ * <ul>
+ * <li>Name: NOAM, Number Of Accessor Methods</li>
+ * <li>Granularity: Class</li>
+ * <li>Default Values: -2 for Interface</li>
+ * </ul>
+ */
 public class NOAMMetric extends AbstractMetricVisitor {
 	private HashMap<String, Long> NOAMMap = new HashMap<>();
+	
+	@Override
+	public boolean visit(TypeDeclaration node) {
+		mPkgNameBuilder.append(node.getName().toString());		
+		
+		// collect fields information
+		HashSet<String> fieldsTypeSet = new HashSet<>();
+		HashSet<String> fieldsNameSet = new HashSet<>();
+		for (FieldDeclaration field: node.getFields()) {
+			//TODO
+			// if all field count?
+//			List<ASTNode> modifiers = field.modifiers();
+//			boolean flag = true;
+//			for (ASTNode modifier: modifiers) {
+//				if (InstanceOfUtil.isModifier(modifier)) {
+//					if (((Modifier)modifier).isStatic() ||
+//							((Modifier)modifier).isFinal()) {
+//						flag = false;
+//						break;
+//					}
+//				}
+//			}
+			String fieldsType = field.getType().toString();
+			for (VariableDeclarationFragment variable: 
+				(List<VariableDeclarationFragment>)field.fragments()) {
+				fieldsTypeSet.add(fieldsType);
+				fieldsNameSet.add(variable.getName().toString());
+			}
+		}
+		
+		if (fieldsNameSet.size() > 0) {
+			countNOAM(fieldsTypeSet, fieldsNameSet, node.getMethods());	
+		}	
+		
+		return false;
+	}
+	
+	private void countNOAM(HashSet<String> fieldsTypeSet, 
+			HashSet<String> fieldsNameSet,
+			MethodDeclaration[] methods) {
+		long result = 0;		
+		for (MethodDeclaration method: methods) {
+			if (method.isConstructor()) {
+				continue;
+			}
+			
+			Type returnType = method.getReturnType2();
+			
+			// what method returns void is not an accessor
+			if (InstanceOfUtil.isPrimitiveType(returnType)) {
+				PrimitiveType type = (PrimitiveType)returnType;
+				if (type.getPrimitiveTypeCode() == PrimitiveType.VOID) {
+					continue;
+				}
+			}
+			
+			if (fieldsTypeSet.contains(returnType.toString())) {
+				List<Statement> statements = method.getBody().statements();
+				if (statements.size() == 1 && 
+						InstanceOfUtil.isReturnStatement(statements.get(0))) {
+					ReturnStatement returnStmt = (ReturnStatement)statements.get(0);
+					Expression returnExp = returnStmt.getExpression();
+					// return this.[Field]
+					if (InstanceOfUtil.isFieldAccess(returnExp)) {
+						FieldAccess fieldAccess = (FieldAccess)returnExp;
+						if (InstanceOfUtil.isThisExpression(fieldAccess.getExpression())) {
+							result++;
+						}
+					}
+					// return [Field]
+					else if (InstanceOfUtil.isSimpleName(returnExp) && 
+							fieldsNameSet.contains(returnExp.toString())) {
+						result++;
+					}
+				}
+			}
+		}
+		NOAMMap.put(mPkgNameBuilder.toString(), result);
+	}
 
 	@Override
 	public HashMap<String, Long> getMetricResult() {
--- src/perfume/test/metric/NOAMTest.java
+++ src/perfume/test/metric/NOAMTest.java
@@ -2,14 +2,16 @@ package perfume.test.metric;
 
 import perfume.metric.AbstractMetricVisitor;
 import perfume.metric.MetricUtil;
-import perfume.metric.visitor.NOAMetricVisitor;
+import perfume.metric.NOAMMetric;
+import perfume.util.LogUtil;
 
 public class NOAMTest {
 
 	public NOAMTest(String projectPath) {
-		AbstractMetricVisitor measurement = new NOAMetricVisitor();
+		AbstractMetricVisitor measurement = new NOAMMetric();
 		MetricUtil.startMetric(
 				projectPath, 
 				measurement);
+		LogUtil.print(measurement.getMetricResult());
 	}
 }
