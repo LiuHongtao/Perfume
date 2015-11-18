2b71914e2bdbb8862d7bf97ce499888d5186bbb3
刘宏韬 <lht91225@gmail.com>
2015-11-18 12:37:28 +0800

    小修改

--- src/perfume/metric/visitor/MAXNESTINGMethodVisitor.java
+++ src/perfume/metric/visitor/MAXNESTINGMethodVisitor.java
@@ -67,7 +67,6 @@ public class MAXNESTINGMethodVisitor extends ASTVisitor {
 	
 	@Override
 	public void endVisit(DoStatement node) {
-		super.endVisit(node);
 		endNest();
 	}
 	
@@ -78,6 +77,7 @@ public class MAXNESTINGMethodVisitor extends ASTVisitor {
 	
 	@Override
 	public void endVisit(ForStatement node) {
+		endNest();
 	}
 	
 	@Override
