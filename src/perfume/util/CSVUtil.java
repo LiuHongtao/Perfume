0d9439fbf77e598fe77e577e17900dcd8df72246
LiuHongtao <lht91225@gmail.com>
2015-12-08 18:24:20 +0800

    修改csv输出方式

--- src/perfume/util/CSVUtil.java
+++ src/perfume/util/CSVUtil.java
@@ -20,46 +20,44 @@ public class CSVUtil {
 			return;
 		}
 		
-		StringBuilder sBuilder = new StringBuilder();
-
-		sBuilder.append("KEY");
-		for (int i = 0; i < times; i++) {
-			sBuilder.append(',');
-			sBuilder.append(metrics[i].getMetricName());
+		//make a file
+		File dir = new File(dirName);
+		if (!dir.exists()) {
+			dir.mkdirs();
 		}
-		sBuilder.append('\n');
 		
-		Iterator iter = metrics[0].getMetricResult().entrySet().iterator();
-		while (iter.hasNext()) {
-			Map.Entry entry = (Map.Entry) iter.next();
-			Object key = entry.getKey();
-			Object val = entry.getValue();
-			
-			sBuilder.append(key);
-			sBuilder.append(',');
-			sBuilder.append(val);
-			
-			for (int i = 1; i < times; i++) {
-				sBuilder.append(',');
-				sBuilder.append(metrics[i].getMetricResult().get(key));
-			}
-			
-			sBuilder.append('\n');
-		}
+		fileName = dirName + fileName + ".csv";
+		File file = new File(fileName);		
 		
 		try {
-			File dir = new File(dirName);
-			if (!dir.exists()) {
-				dir.mkdirs();
-			}
-			
-			fileName = dirName + fileName + ".csv";
-			File file = new File(fileName);
-			
 			FileOutputStream fs;
 			fs = new FileOutputStream(file);
 			PrintStream p = new PrintStream(fs);
-			p.println(sBuilder.toString());
+			
+			p.print("KEY");
+			for (int i = 0; i < times; i++) {
+				p.print(',');
+				p.print(metrics[i].getMetricName());
+			}
+			p.print('\n');
+			
+			Iterator iter = metrics[0].getMetricResult().entrySet().iterator();
+			while (iter.hasNext()) {
+				Map.Entry entry = (Map.Entry) iter.next();
+				Object key = entry.getKey();
+				Object val = entry.getValue();
+				
+				p.print(key);
+				p.print(',');
+				p.print(val);
+				
+				for (int i = 1; i < times; i++) {
+					p.print(',');
+					p.print(metrics[i].getMetricResult().get(key));
+				}
+				
+				p.print('\n');
+			}	
 			p.close();
 			
 			LogUtil.print("output to CSV finish");
@@ -68,6 +66,6 @@ public class CSVUtil {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}				
 	}
 }
